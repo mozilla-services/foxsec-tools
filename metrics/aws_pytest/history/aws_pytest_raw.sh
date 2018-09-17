fa29d794baba6cf0d7cb57ca8447f09a8ec74cdd
Simon Bennetts <psiinon@gmail.com>
2018-09-17 16:33:22 +0200

    Script for copying aws_pytest data to S3

---
+++ metrics/aws_pytest/history/aws_pytest_raw.sh
@@ -0,0 +1,23 @@
+# One off script for copying aws_pytest data from foxsec_results to S3
+
+# Install aws_cli
+pip3 install awscli --upgrade
+
+# Set up the aws credentials
+mkdir ~/.aws
+cat > ~/.aws/credentials <<DELIM
+[default]
+aws_access_key_id = $AWS_ACCESS_KEY
+aws_secret_access_key = $AWS_SECRET_KEY
+DELIM
+
+# Clone repo
+git clone https://$cloudsecBotPat@github.com/mozilla-services/foxsec_results.git
+cd foxsec_results/aws-pytest
+
+# Remove the things we dont want
+rm placeholder */placeholder */one-offs
+rm */*.csv */*.md
+
+# Sync to aws
+aws s3 sync . s3://foxsec-metrics/aws_pytest/raw

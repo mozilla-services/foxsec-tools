7c6811ee2343e1b019384f699640a79d7d35e0eb
Hal Wine <hwine@mozilla.com>
2018-10-19 11:30:29 -0700

    attempt s3 upload & jq install

--- metrics/github/transformers/github_wrapper.sh
+++ metrics/github/transformers/github_wrapper.sh
@@ -1,6 +1,9 @@
 # Wrapper for running the main github transformer
 
 set -o e
+# Install other tools
+apt install -y jq
+
 # Install python tools we need
 pip3 install --upgrade \
   awscli \
@@ -17,10 +20,9 @@ echo -e "\n$githubAPItoken" >./.credentials
 poetry run ./get_branch_protections.py mozilla-frontend-infra
 ls -l *.db.json
 jq . mozilla-frontend-infra.db.json
-id
 
-# bail for now
-exit 33
+# attempt upload
+poetry run make -f moz_scripts/Makefile _full_common
 
 ### Sync todays files
 ##today=`date +%F`

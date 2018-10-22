c795c572aaad69eb1b05be49611724d7b8042cc3
Hal Wine <hwine@mozilla.com>
2018-10-22 07:16:14 -0700

    tidy up

--- metrics/github/transformers/github_wrapper.sh
+++ metrics/github/transformers/github_wrapper.sh
@@ -25,15 +25,3 @@ EOF
 
 # Run the job
 poetry run make -f moz_scripts/Makefile full
-
-### Sync todays files
-##today=`date +%F`
-##mkdir s3bucket
-##mkdir out
-##aws s3 cp s3://foxsec-metrics/pyup/raw/$today.pyup_dash.json s3bucket/
-##
-### Run transformer for today
-##python3 transformers/pyup.py -s s3bucket/ -d out/ -D "$today"
-## 
-### Write todays file to aws
-##aws s3 cp out/$today s3://foxsec-metrics/pyup/pyup_json/$today

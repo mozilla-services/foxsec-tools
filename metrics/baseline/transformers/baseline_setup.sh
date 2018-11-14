cce93c43edab340e52afa8acf8b6eab4c677af5b
Simon Bennetts <psiinon@gmail.com>
2018-11-14 16:12:00 +0000

    Put aws creds back in

--- metrics/baseline/transformers/baseline_setup.sh
+++ metrics/baseline/transformers/baseline_setup.sh
@@ -3,6 +3,14 @@
 # Install aws_cli
 pip3 install awscli --upgrade
 
+# Set up the aws credentials
+mkdir ~/.aws
+cat > ~/.aws/credentials <<DELIM
+[default]
+aws_access_key_id = $AWS_ACCESS_KEY
+aws_secret_access_key = $AWS_SECRET_KEY
+DELIM
+
 mkdir s3bucket
 aws s3 sync s3://foxsec-metrics/baseline/ s3bucket/
 

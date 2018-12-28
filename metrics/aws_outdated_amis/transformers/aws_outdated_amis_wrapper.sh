48ff3a354293d9a2965deeeb20b9adac59dd5de7
AJ Bahnken <aj@ajvb.me>
2018-12-28 10:31:05 -0800

    Fetch raw pytest results from S3

--- metrics/aws_outdated_amis/transformers/aws_outdated_amis_wrapper.sh
+++ metrics/aws_outdated_amis/transformers/aws_outdated_amis_wrapper.sh
@@ -3,18 +3,12 @@
 # Install aws_cli
 pip3 install awscli --upgrade
 
-# Clone repo
-git clone --depth 1 https://$cloudsecBotPat@github.com/mozilla-services/foxsec-results.git
-resdir=foxsec-results/aws-outdated-amis
+today=$(date +%F)
+tdir=$(mktemp -d)
 
-today=`date +%F`
-
-for filename in $resdir/*/*$today.json; do
-	acc=$(basename $(dirname $filename))
-	aws s3 cp $filename s3://foxsec-metrics/aws_pytest/raw/$acc/
-done
+aws s3 sync s3://foxsec-metrics/aws_pytest/raw/ $tdir --exclude "*" --include "*${today}.json"
 
 # Perform the transformation
 mkdir out
-./aws_outdated_amis.py -s $resdir -d out -D $today
+./aws_outdated_amis.py -s $tdir -d out -D $today
 aws s3 cp out/$today s3://foxsec-metrics/aws_outdated_amis/aws_outdated_amis_json/
--- metrics/aws_pytest/transformers/aws_pytest_wrapper.sh
+++ metrics/aws_pytest/transformers/aws_pytest_wrapper.sh
@@ -3,18 +3,12 @@
 # Install aws_cli
 pip3 install awscli --upgrade
 
-# Clone repo
-git clone --depth 1 https://$cloudsecBotPat@github.com/mozilla-services/foxsec-results.git
-resdir=foxsec-results/aws-pytest
+today=$(date +%F)
+tdir=$(mktemp -d)
 
-today=`date +%F`
-
-for filename in $resdir/*/*$today.json; do
-	acc=$(basename $(dirname $filename))
-	aws s3 cp $filename s3://foxsec-metrics/aws_pytest/raw/$acc/
-done
+aws s3 sync s3://foxsec-metrics/aws_pytest/raw/ $tdir --exclude "*" --include "*${today}.json"
 
 # Perform the transformation
 mkdir out
-./pytest_services_metrics.py -s $resdir -d out -D $today
+./pytest_services_metrics.py -s $tdir -d out -D $today
 aws s3 cp out/$today s3://foxsec-metrics/aws_pytest/aws_service_json/

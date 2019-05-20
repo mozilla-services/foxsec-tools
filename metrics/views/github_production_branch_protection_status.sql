002c2c77918e66fbe9590f9c313f6b4eb7821473
Hal Wine <hwine@mozilla.com>
2019-05-20 14:43:58 -0700

    Backup

---
+++ metrics/views/github_production_branch_protection_status.sql
@@ -0,0 +1,11 @@
+CREATE VIEW foxsec_metrics.github_production_branch_protection_status AS
+SELECT
+  "Protected"
+, "Service"
+, "meta"."org" "Org"
+, "meta"."repo" "Repo"
+, "branch"."branch" "Branch"
+, "date"
+FROM
+  (foxsec_metrics.metadata_repo_parsed meta
+INNER JOIN foxsec_metrics.all_default_branch_protection_status branch ON (("meta"."org" = "branch"."org") AND ("meta"."repo" = "branch"."repo")))

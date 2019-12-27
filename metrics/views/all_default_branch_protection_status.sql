9c0c974b74b8ab0fb153938a6c3ec84d3eabf13f
Hal Wine <hwine@mozilla.com>
2019-12-27 15:37:29 -0800

    Don't process archived repos
    This query change ignores archived repos. By definition, they are fully
    protected.

--- metrics/views/all_default_branch_protection_status.sql
+++ metrics/views/all_default_branch_protection_status.sql
@@ -1,12 +1,22 @@
 CREATE VIEW foxsec_metrics.all_default_branch_protection_status AS
+WITH
+  active_repos AS (
+   SELECT
+     "url" "Prefix"
+   , "date"
+   FROM
+     foxsec_metrics.github_object
+   WHERE (("body"."archived" IS NOT NULL) AND ("body"."archived" = false))
+) 
 SELECT
   "body"."name" "Name"
 , "body"."protected" "Protected"
-, "URL"
+, "url"
 , "split_part"("url", '/', 3) "Org"
 , "split_part"("url", '/', 4) "Repo"
 , "split_part"("url", '/', 6) "Branch"
-, "date"
+, "branch"."date"
 FROM
-  github_object
+  (foxsec_metrics.github_object branch
+LEFT JOIN active_repos ON (("active_repos"."date" = "branch"."date") AND ("strpos"("branch"."url", "active_repos"."prefix") = 1)))
 WHERE ("body"."protected" IS NOT NULL)

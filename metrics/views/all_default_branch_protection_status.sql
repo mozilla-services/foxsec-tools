b31b85491d251e21b7b18b549d7132746471447d
Hal Wine <hwine@mozilla.com>
2020-01-17 11:52:26 -0800

    Roll back ignore archived repos
    Undo the relevant part of 9c0c974b74b8ab0fb153938a6c3ec84d3eabf13f
    that changed metrics/views/all_default_branch_protection_status.sql
    
    The change "worked" in terms of producing the correct data, but changed
    the query times to the point that it would not run in production queries
    (where this is but one table). This change will return us to reporting
    on archived repos. :(
    
    Rough numbers on this query:
    exclude archived: 34 sec, 20 GB scanned <= Giga
    include archived:  4 sec, 22 MB scanned <= Mega

--- metrics/views/all_default_branch_protection_status.sql
+++ metrics/views/all_default_branch_protection_status.sql
@@ -1,22 +1,12 @@
 CREATE VIEW foxsec_metrics.all_default_branch_protection_status AS
-WITH
-  active_repos AS (
-   SELECT
-     "url" "Prefix"
-   , "date"
-   FROM
-     foxsec_metrics.github_object
-   WHERE (("body"."archived" IS NOT NULL) AND ("body"."archived" = false))
-) 
 SELECT
   "body"."name" "Name"
 , "body"."protected" "Protected"
-, "url"
+, "URL"
 , "split_part"("url", '/', 3) "Org"
 , "split_part"("url", '/', 4) "Repo"
 , "split_part"("url", '/', 6) "Branch"
-, "branch"."date"
+, "date"
 FROM
-  (foxsec_metrics.github_object branch
-LEFT JOIN active_repos ON (("active_repos"."date" = "branch"."date") AND ("strpos"("branch"."url", "active_repos"."prefix") = 1)))
+  foxsec_metrics.github_object
 WHERE ("body"."protected" IS NOT NULL)
